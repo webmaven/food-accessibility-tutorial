5d5f4e177b213bc27c598d5f3add72c7a8b78135
Michael R. Bernstein <michael@fandomhome.com>
2015-07-11 11:24:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
+// Activity #3
 
+$(document).ready(function() {
 
 
 	var map = L.map('map').setView([35.104602, -106.628414], 11);
@@ -11,21 +12,52 @@ $(document).ready(function() {
 	}).addTo(map);
 
 
-    var url = "data/retail_grocery.geojson";
-    $.ajax({
-        url: url,
-        dataType: 'json',
-        success: function (data) {
+	var url = "data/retail_grocery.geojson";
+	$.ajax({
+	    url: url,
+	    dataType: 'json',
+	    success: function (data) {
+	    	groceryGeoJson = data;
+	    	
+	    	geojson = new L.GeoJSON(data, {
+	    		
+	    		onEachFeature: function(feature, layer){
+	    			layer.bindPopup(feature.properties.Name);
 
-		geojson = new L.GeoJSON(data, {
+	    		}
+	    	});
+	    	geojson.addTo(map);
+	      	
+	    }
 
-		onEachFeature: function(feature, layer){
-			layer.bindPopup("poverty: " + Math.floor(100*parseInt(feature.properties.poverty)/(feature.properties.population)) + "%");
-                }
-            });
-            geojson.addTo(map);
+	    
+	});
 
-        }
+		// Activity 3 - see the alternate_citysdk_ajax _call.js for the request used to grab the census.geojson data below
+
+	var url = "../../data/census.geojson";
+	$.ajax({
+	    url: url,
+	    dataType: 'json',
+	    success: function (data) {
+	    	
+	    	geojson = new L.GeoJSON(data, {
+	    		
+	    		onEachFeature: function(feature, layer){
+	    			layer.bindPopup("poverty: " + Math.floor(100*parseInt(feature.properties.poverty)/(feature.properties.population)) + "%");
+
+	    		}
+	    	});
+	    	geojson.addTo(map);
+	      	
+	    }
+
+	    
+	});
+
+	//// the end section you'd replace if making the call directly to the citySDK servers
+
+	
 
 
-    });
+});
