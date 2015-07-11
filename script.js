f02786c18d8bd99089e903a6209b9836aaa0f5ca
Michael R. Bernstein <michael@fandomhome.com>
2015-07-11 11:30:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Activity #3
+// Activity #4
 
 $(document).ready(function() {
 
+
+	// Activity 1
 
 	var map = L.map('map').setView([35.104602, -106.628414], 11);
 
@@ -12,7 +14,10 @@ $(document).ready(function() {
 	}).addTo(map);
 
 
+	// Activity 2
+
 	var url = "data/retail_grocery.geojson";
+	var groceryGeoJson, tractGeoJson;
 	$.ajax({
 	    url: url,
 	    dataType: 'json',
@@ -35,21 +40,40 @@ $(document).ready(function() {
 
 		// Activity 3 - see the alternate_citysdk_ajax _call.js for the request used to grab the census.geojson data below
 
+
+	var threshold = 0.25;	//activity 4
+
 	var url = "data/census.geojson";
+	
 	$.ajax({
 	    url: url,
 	    dataType: 'json',
 	    success: function (data) {
-	    	
+	    	blockCount = data.features.length;
 	    	geojson = new L.GeoJSON(data, {
-	    		
+
+	    	/////// Activity 4 - add the style	element
+	    		style: function (feature) {
+		  		if ((feature.properties.poverty)/(feature.properties.population) > threshold) {
+		  				return {color: 'red'};
+		  		}
+		  		else {
+		  			return {color: 'green'};
+		  			
+		  		}
+        
+    	},
+
+    	////////
 	    		onEachFeature: function(feature, layer){
 	    			layer.bindPopup("poverty: " + Math.floor(100*parseInt(feature.properties.poverty)/(feature.properties.population)) + "%");
 
+
 	    		}
 	    	});
+
 	    	geojson.addTo(map);
-	      	
+	
 	    }
 
 	    
@@ -57,7 +81,8 @@ $(document).ready(function() {
 
 	//// the end section you'd replace if making the call directly to the citySDK servers
 
-	
+	   
 
 
 });
+
